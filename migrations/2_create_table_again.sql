86c6afa8ade6089727e8d6f5986a25e8e0db99aa
Matthew Woods <mxttwoods@gmail.com>
2022-02-02 23:17:46 -0500

    fix sql

--- migrations/2_create_table_again.sql
+++ migrations/2_create_table_again.sql
@@ -1,14 +1,6 @@
-CREATE TABLE `dept_emp` (
-    `emp_no` INT NOT NULL,
-    `dept_no` CHAR(4) NOT NULL,
-    `from_date` DATE NOT NULL,
-    `to_date` DATE NOT NULL,
-    PRIMARY KEY (`emp_no` , `dept_no`),
-    KEY `dept_no` (`dept_no`),
-    CONSTRAINT `dept_emp_ibfk_1` FOREIGN KEY (`emp_no`)
-        REFERENCES `employees` (`emp_no`)
-        ON DELETE CASCADE,
-    CONSTRAINT `dept_emp_ibfk_2` FOREIGN KEY (`dept_no`)
-        REFERENCES `departments` (`dept_no`)
-        ON DELETE CASCADE
+CREATE TABLE `locations` (
+    `location_no` CHAR(4) NOT NULL,
+    `location_name` VARCHAR(40) NOT NULL,
+    PRIMARY KEY (`location_no`),
+    UNIQUE KEY `location_name` (`location_name`)
 )  ENGINE=INNODB DEFAULT CHARSET=UTF8MB4 COLLATE = UTF8MB4_0900_AI_CI;
